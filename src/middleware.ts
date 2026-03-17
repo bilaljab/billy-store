b44d97ec4e2e396658d704a9f9be983571864beb
Bilal Jabasini <bilal.jab.2000@gmail.com>
2026-03-17 18:56:06 +0300

    fix: add async to middleware function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyToken } from '@/lib/auth';
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const res = NextResponse.next();
 
